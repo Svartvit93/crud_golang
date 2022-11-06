4c3ba9fbc029f7b665883658493faaf60182fdd6
Nils Part <nils.part@wow-group.com>
2022-11-06 23:06:45 +0100

    added som structs and funcs

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/svartvit93/crud_go/bootstrap"
+	"github.com/svartvit93/crud_go/repository"
+)
+
+type Repository repository.Repository
+
+func main() {
+	app := fiber.New()
+	boostrap.InitializeApp(app)
+}
---
+++ repository/routes.go
@@ -0,0 +1,9 @@
+package repository
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func (repo *Repository) SetupRoutes(app *fiber.App) {
+
+}
